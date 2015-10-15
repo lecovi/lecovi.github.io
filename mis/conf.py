010671a21ce529172c57ccdcf85038a2994def0f
Leandro E. Colombo Viña (XPS) <colomboleandro@bitson.com.ar>
2015-10-15 20:45:44 -0300

    Adding index to CFP, IFTS & bitson

--- mis/conf.py
+++ mis/conf.py
@@ -933,7 +933,7 @@ MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra']
 # (translatable)
 EXTRA_HEAD_DATA = """
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
-<link href='assets/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
+<link href='/assets/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
 """
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
---
+++ mis/stories/bitson-index.rst
@@ -0,0 +1,23 @@
+.. title: Cooperativa de Trabajo BITSON Ltda.
+.. slug: bitson
+.. date: 2015-10-15 19:37:47 UTC-03:00
+.. tags:
+.. category:
+.. link: bitson
+.. description:
+.. type: text
+.. hidetitle: true
+
+.. class:: h2 align-center
+
+Cooperativa de Trabajo BITSON Ltda.
+
+
+.. class:: lead align-center
+
+    Adolfo Alsina 1760 10° Of. 42, Ciudad Autónoma de Buenos Aires
+    4381 2971
+
+Algunos de nuestros trabajos:
+
+- :doc:`Programación con Software Libre <bitson/prog-sl>`
