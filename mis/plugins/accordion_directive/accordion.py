97c92843d945a59761d1ae4eb29a0cb8a7b0adae
Leandro E. Colombo Viña (XPS) <colomboleandro@bitson.com.ar>
2015-10-24 13:17:06 -0300

    Changing RSS_ to FEED_ & removing copyright symbol & ñ from plugins

--- mis/plugins/accordion_directive/accordion.py
+++ mis/plugins/accordion_directive/accordion.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright © 2015 Manuel Kaufmann & Leandro E. Colombo Viña
+# Copyright 2015 Manuel Kaufmann & LeCoVi
 
 # Permission is hereby granted, free of charge, to any
 # person obtaining a copy of this software and associated
@@ -23,8 +23,6 @@
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-import uuid
 
 from docutils import nodes
 from docutils.parsers.rst import Directive, directives
--- mis/plugins/collapse_directive/collapse.py
+++ mis/plugins/collapse_directive/collapse.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright © 2015 Manuel Kaufmann & Leandro E. Colombo Viña
+# Copyright 2015 Manuel Kaufmann & LeCoVi
 
 # Permission is hereby granted, free of charge, to any
 # person obtaining a copy of this software and associated
