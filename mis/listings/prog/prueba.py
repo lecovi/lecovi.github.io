81b3017476429853ae63263f67207f0e19af5a90
Leandro E. Colombo Viña (XPS) <colomboleandro@bitson.com.ar>
2016-05-04 18:03:18 -0300

    PROG: fixing ascii problems

--- mis/listings/prog/prueba.py
+++ mis/listings/prog/prueba.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-# función que calcula el promedio
-# de dos números
+# funcion que calcula el promedio
+# de dos numeros
 def promedio(numero1, numero2):
     prom = (numero1 + numero2) / 2
 
     return prom
-    
+
 print(promedio(8, 5))
 
 p = int(input("ingrese un num: " ))
@@ -17,9 +17,3 @@ t = promedio(p, q)
 print(t)
 
 print(promedio(promedio(10,20), promedio(30,40)))
-
-
-    
-
-
-
